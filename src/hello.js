4ec4fefc539da6508a44ba0597b5570d5f2a1917
Vivek Kumar <mail2vivek.net@gmail.com>
2022-11-04 20:44:38 +0530

    added mode in webpack

--- src/hello.js
+++ src/hello.js
@@ -3,3 +3,6 @@ var x = 100;
 debugger;
 
 console.log(x);
+
+const x = 10;
+x = 15;
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   // devtool: 'source-map',
-  // mode: 'development',
+  mode: 'development',
 	entry: "./src/index.js",
 	output: {
 		path: path.resolve(__dirname, "dist"),
